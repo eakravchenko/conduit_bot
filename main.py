82be100d26c194acbe8f900a33caad6228226512
Egor Kravchenko <kravchenkoea02@gmail.com>
2022-06-30 19:41:45 +0300

    echo bot

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
+import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-print(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+bot.polling(none_stop=True, interval=0)
+
